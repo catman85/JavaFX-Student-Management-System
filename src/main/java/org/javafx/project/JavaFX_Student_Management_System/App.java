02c9455bf371dfcb236c2b09fa37434329b1b462
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-09-07 15:40:50 +0300

    Converting to Maven Project

---
+++ src/main/java/org/javafx/project/JavaFX_Student_Management_System/App.java
@@ -0,0 +1,13 @@
+package org.javafx.project.JavaFX_Student_Management_System;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

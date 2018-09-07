f53bcfdbfc976be93da81098bb4bb7a0e0792a63
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-09-07 17:35:17 +0300

    Added Borderless scene control

--- src/main/java/org/javafx/studentsmanagementsystem/application/Main.java
+++ src/main/java/org/javafx/studentsmanagementsystem/application/Main.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import main.java.com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
+import main.java.org.javafx.studentsmanagementsystem.controller.TopBar;
 import main.java.org.javafx.studentsmanagementsystem.model.SQLiteJDBC;
 
 public class Main extends Application {
@@ -12,12 +16,42 @@ public class Main extends Application {
 	public final static String FXMLS = "/fxml/";
 	public final static String IMAGES = "/images/";
 	
+	/** The Top Bar of the Application */
+	public static TopBar topBar;
+	
+	public static BorderPane root;
+	
+	public static Stage window;
+	
+	/** The scene. */
+	public static BorderlessScene borderlessScene;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource(FXMLS + "main.fxml"));
+		
+		//TopBar
+		topBar = new TopBar();
+		
+		//Content
+		Parent content = FXMLLoader.load(getClass().getResource(FXMLS + "main.fxml"));
+		
+		//Root 
+		root = new BorderPane();
+		root.setStyle("-fx-background-color:#202020");
+		root.setTop(topBar);
+		root.setCenter(content);
+		
+		//Primary Stage
+		window = primaryStage;
 		primaryStage.setTitle("Students Management System");
-		primaryStage.setScene(new Scene(root, 700, 500));
+		
+		// Borderless Scene
+		borderlessScene = new BorderlessScene(window, StageStyle.UNDECORATED, root, 700, 500);
+		borderlessScene.setMoveControl(topBar);
+		primaryStage.setScene(borderlessScene);
 		primaryStage.show();
+		
+		//Create the Database
 		SQLiteJDBC.makeDb();
 	}
 	
